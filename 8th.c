da63b6037b7e806914ab0bf77984ace2c5557748
Rupam Maity <rupammaity2349@gmail.com>
2026-08-01 23:34:42 +0530

    WAP to calculate area and volume of a cube
    Implement a program to calculate the area and volume of a cube based on user input.

---
+++ 8th.c
@@ -0,0 +1,10 @@
+// WAP to calculate area and volume of a cube
+
+#include<stdio.h>
+void main()
+{
+    float a;
+    printf("Enter side length: ");
+    scanf("%f", &a);
+    printf("Area = %.2f \nVolume = %.2f",6*a*a,a*a*a);
+}
