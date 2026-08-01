6019217ae87bf6fdd9c5df8989cb59b00dd1ea61
Rupam Maity <rupammaity2349@gmail.com>
2026-08-01 23:38:30 +0530

    WAP to convert hours to minutes

---
+++ 10th.c
@@ -0,0 +1,10 @@
+// WAP to convert hours to minutes
+
+#include<stdio.h>
+void main()
+{
+    int h;
+    printf("Enter hour(s): ");
+    scanf("%d", &h);
+    printf("%d hour(s) = %d minutes", h, h*60);
+}
