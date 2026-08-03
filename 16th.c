5b8c593dd01ab306d53e013fdabc8f8abe0eaac6
Rupam Maity <rupammaity2349@gmail.com>
2026-08-03 22:52:45 +0530

    WAP to check greatest between 2 Nos.

---
+++ 16th.c
@@ -0,0 +1,14 @@
+// WAP to check greatest between 2 Nos.
+#include <stdio.h>
+void main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    if (a > b)
+        printf("%d is the greatest", a);
+    else if (a < b)
+        printf("%d is the greatest", b);
+    else
+        printf("Both are equal");
+}
