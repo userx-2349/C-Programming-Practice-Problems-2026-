883b28e1333beae6660e9d87ab65e18582c7f966
Rupam Maity <rupammaity2349@gmail.com>
2026-08-03 22:48:42 +0530

    WAP to convert months to days

---
+++ 13th.c
@@ -0,0 +1,10 @@
+// WAP to convert months to days
+
+#include<stdio.h>
+void main()
+{
+    int m;
+    printf("Enter month(s): ");
+    scanf("%d", &m);
+    printf("%d month(s) = %d days",m,m*30);
+}
